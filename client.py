5e2ef25abb3e8fe5865dd7ab9c82621cdc88ebeb
sayyyho <323psh@naver.com>
2023-06-03 17:16:16 +0900

    add : client.py / server.py [server - client finished]

--- client.py
+++ client.py
@@ -1,20 +1,49 @@
-import socket
+from socket import *
+import threading
+import time
 
-host = '127.0.0.1'
-port = 9123
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 9123
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((host, port))
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((SERVER_IP, SERVER_PORT))
+
+print(f"연결완료 !!\nip : {SERVER_IP}, port : {SERVER_PORT}")
+print("if you want to close server, please enter <<< quit >>>")
+
+def send(client_socket):
+    while True:
+        msg = input("(you) >> ")
+        client_socket.sendall(msg.encode("utf-8"))
+        if msg == "quit": 
+            print("socket is closed. please enter key <<< control + c or control + d >>>")        
+            client_socket.close()
+            break
+        
+def receive(client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode("utf-8")
+            print("\n[from server]")
+            if not msg or msg == "quit": 
+                print("socket is closed. please enter key <<< control + c or control + d >>>")        
+                client_socket.close()
+                break
+            print(f">> {msg}\n(you) >> ", end="")
+        except:
+            pass
+
+sending = threading.Thread(target=send, args=(client_socket,)) # 틀어 놓고 병렬 대기?
+receiving = threading.Thread(target=receive, args=(client_socket,))
+sending.daemon = True
+receiving.daemon = True
+sending.start()
+receiving.start()
 
 while True:
-    msg = input("input msg : ")
-    client_socket.sendall(msg.encode("utf-8"))
-    if msg == "quit":
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        client_socket.close()
         break
-    receive_data = client_socket.recv(1024)
-    print(f"receive : {receive_data.decode('utf-8')}")
-
-client_socket.close()
-print("접속을 종료합니다.")
-
-# https://sungmin-joo.tistory.com/56
+print("\n프로그램 종료")
--- server.py
+++ server.py
@@ -1,26 +1,59 @@
-import socket
+from socket import *
+import threading
+import time
+
 
 host = "127.0.0.1"
 port = 9123
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 server_socket.bind((host, port))
 
+print("server에서 listen 중..")
 server_socket.listen()
 
-client_socket, addr = server_socket.accept()
+conn, addr = server_socket.accept()
 
-print("클라이언트의 정보 : ", addr)
+print(f"연결 완료 !!!\nuser_info : {addr[0]} : {addr[1]}")
+print("if you want to close server, please enter <<< quit >>>")
+
+def send(client_socket):
+    while True:
+        msg = input("(you) >> ")
+        client_socket.sendall(msg.encode("utf-8"))
+        if msg == "quit": 
+            print("socket is closed. please enter key <<< control + c or control + d >>>")        
+            client_socket.close()
+            break
+        
+def receive(client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode("utf-8")
+            print("\n[from client]")
+            if not msg or msg == "quit": 
+                print("socket is closed. please enter key <<< control + c or control + d >>>")        
+                client_socket.close()
+                break
+            print(f">> {msg}\n(you) >> ", end="")
+        except:
+            pass
+        
+sending = threading.Thread(target=send, args=(conn,)) # 틀어 놓고 병렬 대기?
+receiving = threading.Thread(target=receive, args=(conn,))
+sending.daemon = True
+receiving.daemon = True
+sending.start()
+receiving.start()
+
+server_socket.close()
 
 while True:
-    msg = client_socket.recv(1024).decode("utf-8")
-    if msg == "quit": 
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        conn.close()
+        server_socket.close()
         break
-    print(f"recieve : {msg}")
-    client_socket.sendall(msg.encode())
-
-print("접속을 종료합니다.")
-client_socket.close()
-server_socket.close()
+print("\n프로그램 종료")
